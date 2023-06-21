fdd3c7e5a7f25a19d845e1c0080713db56be4dc7
Иван Далеченко <ivandalechenko@gmail.com>
2023-06-21 19:05:02 +0300

    adde smooth animation on questions

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,16 @@
-var elements = document.querySelectorAll(".question_arrow");
+var elements = document.querySelectorAll(".question");
 for (var i = 0; i < elements.length; i++) {
     elements[i].onclick = function () {
-        this.classList.toggle('show')
-        this.parentNode.querySelector(".text .hidden").classList.toggle('dnone')
+        this.querySelector(".question_arrow").classList.toggle('show')
+        var elBlock = this.querySelector(".text .hidden")
+        if (elBlock.style.height === "0px") {
+            elBlock.style.height = `${elBlock.scrollHeight}px`
+            elBlock.style.marginTop = `8px`
+        } else {
+            elBlock.style.height = `${elBlock.scrollHeight}px`;
+            window.getComputedStyle(elBlock, null).getPropertyValue("height");
+            elBlock.style.height = "0";
+            setTimeout(() => { elBlock.style.marginTop = `0`; }, 500);
+        }
     };
 }
