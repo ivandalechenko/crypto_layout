1b577e34b3ac9486bc3a70a01466daf897197663
Иван Далеченко <ivandalechenko@gmail.com>
2023-06-29 22:42:37 +0300

    fix tutorial

---
+++ js/auth.js
@@ -0,0 +1,13 @@
+document.getElementById("login_switcher").onclick = switch_to_sign_up
+function switch_to_sign_up() {
+    console.log('sign_up')
+}
+
+document.getElementById("login_switcher").onclick = switch_to_log_in
+function switch_to_log_in() {
+    console.log('login')
+}
+
+function switch_to_code() {
+    console.log('code')
+}
