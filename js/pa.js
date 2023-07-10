43650e7ec9627d8548fa146071ffc23648b5864b
Иван Далеченко <ivandalechenko@gmail.com>
2023-07-10 18:39:04 +0300

    added some blocks for pa

--- js/pa.js
+++ js/pa.js
@@ -1,3 +1,12 @@
-// function get_profile() {
-//     document.getElementById('content')
-// }
+document.getElementById('burger_opener').onclick = show_menu
+function show_menu() {
+    document.getElementById('burger_opener').classList.add('dnone')
+    document.getElementById('burger_closer').classList.remove('dnone')
+    document.getElementById('sidebar').style.height = `${document.getElementById('sidebar').scrollHeight}px`
+}
+document.getElementById('burger_closer').onclick = hide_menu
+function hide_menu() {
+    document.getElementById('sidebar').style.height = ``
+    document.getElementById('burger_closer').classList.add('dnone')
+    document.getElementById('burger_opener').classList.remove('dnone')
+}
