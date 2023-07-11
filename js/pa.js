c33a6b6c93c096748efedb79812561bdeece64fc
Иван Далеченко <ivandalechenko@gmail.com>
2023-07-11 17:27:50 +0300

    added ref

--- js/pa.js
+++ js/pa.js
@@ -18,19 +18,21 @@ function hide_modal() {
     setTimeout(() => { document.getElementById("modal").classList.add('dnone') }, 500)
 }
 
-// profile_opener
-
 function show(el) {
     el.classList.remove('dnone')
+    // el.classList.remove('onone')
     setTimeout(remove_onone, 50, el)
 }
 
 function hide_all(target_page) {
     pages = document.querySelectorAll('.page')
+    console.log(pages)
     for (var i = 0; i < pages.length; i++) {
         if (pages[i].id != target_page.id) {
-            pages[i].classList.add('onone')
-            setTimeout(add_dnone, 500, pages[i])
+            if (!pages[i].classList.contains('onone')) {
+                pages[i].classList.add('onone')
+                setTimeout(add_dnone, 500, pages[i])
+            }
         }
     }
 }
@@ -41,18 +43,34 @@ function remove_onone(el) {
     el.classList.remove('onone')
 }
 
+function set_active_menu_element(el) {
+    menu_elements = document.querySelectorAll('.navigation_element')
+    for (var i = 0; i < menu_elements.length; i++) {
+        if (menu_elements[i].id != el.id) {
+            menu_elements[i].classList.remove('active')
+        }
+    }
+    el.classList.add('active')
+}
+
+
 document.getElementById('burger_opener').onclick = show_menu
-
 document.getElementById('burger_closer').onclick = hide_menu
-
 document.getElementById('modal_closer').onclick = hide_modal
 document.getElementById('add_api_keys_button').onclick = show_modal
 
 document.getElementById("manage_tarif_opener").onclick = () => {
-    hide_all(document.getElementById("page_manage_tarif"))
-    show(document.getElementById("page_manage_tarif"))
+    set_active_menu_element(document.getElementById("manage_tarif_opener"))
+    hide_all(document.getElementById("manage_tarif_page"))
+    show(document.getElementById("manage_tarif_page"))
 }
 document.getElementById("profile_opener").onclick = () => {
-    hide_all(document.getElementById("page_profile"))
-    show(document.getElementById("page_profile"))
+    set_active_menu_element(document.getElementById("profile_opener"))
+    hide_all(document.getElementById("profile_page"))
+    show(document.getElementById("profile_page"))
+}
+document.getElementById("ref_opener").onclick = () => {
+    set_active_menu_element(document.getElementById("ref_opener"))
+    hide_all(document.getElementById("ref_page"))
+    show(document.getElementById("ref_page"))
 }
