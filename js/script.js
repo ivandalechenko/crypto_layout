00ea341b4a47dd9dd0bcd368f2b75c8b6c89471d
Иван Далеченко <ivandalechenko@gmail.com>
2023-07-11 14:41:38 +0300

    fix loader

--- js/script.js
+++ js/script.js
@@ -177,7 +177,4 @@ lozad('.lozad', {
 }).observe()
 
 
-function hide_loader() {
-    document.getElementById('loader').style.opacity = 0;
-    setTimeout(() => { document.getElementById('loader').classList.add('dnone') }, 600)
-}
+
