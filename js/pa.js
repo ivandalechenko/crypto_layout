d0e6fdb3fa9cfa04c5eef8ad6075abc5f3e9d1ca
Иван Далеченко <ivandalechenko@gmail.com>
2023-07-11 20:46:43 +0300

    added some modal

--- js/pa.js
+++ js/pa.js
@@ -9,13 +9,20 @@ function hide_menu() {
     document.getElementById('burger_opener').classList.remove('dnone')
 }
 
-function show_modal() {
+function show_modal(el) {
+    el.classList.remove('dnone')
     document.getElementById("modal").classList.remove('dnone')
     setTimeout(() => { document.getElementById("modal").classList.remove('onone') }, 50)
 }
 function hide_modal() {
     document.getElementById("modal").classList.add('onone')
-    setTimeout(() => { document.getElementById("modal").classList.add('dnone') }, 500)
+    setTimeout(() => {
+        document.getElementById("modal").classList.add('dnone')
+        modal_inners = document.querySelectorAll('.modal_inner')
+        for (var i = 0; i < modal_inners.length; i++) {
+            modal_inners[i].classList.add('dnone')
+        }
+    }, 500)
 }
 
 function show(el) {
@@ -53,11 +60,22 @@ function set_active_menu_element(el) {
     el.classList.add('active')
 }
 
-
+checkboxes = document.querySelectorAll('.checkbox_wrapper')
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].onclick = toggle_checked(checkboxes[i])
+}
+function toggle_checked(el) {
+    return (event) => {
+        el.classList.toggle('checked')
+    }
+}
 document.getElementById('burger_opener').onclick = show_menu
 document.getElementById('burger_closer').onclick = hide_menu
-document.getElementById('modal_closer').onclick = hide_modal
-document.getElementById('add_api_keys_button').onclick = show_modal
+
+modal_closers = document.querySelectorAll('.modal_closer')
+for (var i = 0; i < modal_closers.length; i++) {
+    modal_closers[i].onclick = hide_modal
+}
 
 document.getElementById("manage_tarif_opener").onclick = () => {
     set_active_menu_element(document.getElementById("manage_tarif_opener"))
@@ -73,4 +91,12 @@ document.getElementById("ref_opener").onclick = () => {
     set_active_menu_element(document.getElementById("ref_opener"))
     hide_all(document.getElementById("ref_page"))
     show(document.getElementById("ref_page"))
+}
+
+document.getElementById("pay_opener").onclick = () => {
+    show_modal(document.getElementById("inner_pay"))
+}
+
+document.getElementById('add_api_opener').onclick = () => {
+    show_modal(document.getElementById("inner_add_api"))
 }
